2f106373c5e2f3890699134245663eae6531b22d
soarescomputer96 <162142174+soarescomputer96@users.noreply.github.com>
2024-04-05 11:25:54 -0300

    Add files via upload
    ALUNOS MATRICULA ATIVIDADE JOSIVALDO

---
+++ teste.py
@@ -0,0 +1,13 @@
+class  aluno():
+    def __init__(self, nome, idade, sexo, matricula)
+        self.nome = nome
+        self.idade = idade 
+        self.sexo = sexo 
+        self.matricula = matricula
+
+       if __name__ = "__main__" :
+aluno1 = aluno ("miguel", "17", "masculino", "001817")
+print (aluno1.nome)
+print (aluno1.idade)
+print (aluno1.sexo)
+print (aluno1.matricula)
